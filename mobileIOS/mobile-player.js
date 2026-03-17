caf40e2a0c9ec7b33e7b7cbf682223e3045b3a72
WADBB <kiki8.fs@gmail.com>
2026-03-17 11:10:00 +0100

    Update mobile-player.js

--- mobileIOS/mobile-player.js
+++ mobileIOS/mobile-player.js
@@ -10,6 +10,7 @@ let availableAudioTracks = [];
 let availableSubtitles = [];
 let availableQualities = [];
 let cleanupFunctions = [];
+const VIXSRC_URL = "vixsrc.to"; // Sostituisci con il tuo dominio
 
 // ============ PLAYER FUNCTIONS ============
 async function openMobilePlayer(item) {
